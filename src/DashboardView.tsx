ab13acadda4b77e268f808c9c8ea37639a00feeb
danis <amaury.danis-cousandier@epitech.eu>
2023-05-29 12:56:31 +0200

    dashboard css + map centered + new assets

--- src/DashboardView.tsx
+++ src/DashboardView.tsx
@@ -1,23 +1,36 @@
 import * as React from 'react';
+import { ImageBackground } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Progress, Box, Text, Heading, Stack } from 'native-base';
+import { Progress, Box, Text, Heading, Stack, Button } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
 
 export default function DashboardView() {
+	const navigation = useNavigation();
+
 	return(
 		<SafeAreaView>
-			<Stack>
-				<Box >
+				<Stack m={4} space={4}>
 					<Heading>Today's average values</Heading>
-					<Text>Temperature</Text>
-					<Progress rounded="0" value={65} mx='4' />
-					<Text>Humidity</Text>
-					<Progress rounded="0" value={50} mx='4' />
-					<Text>Water quality</Text>
-					<Progress rounded="0" value={80} mx='4' />
-					<Text>Water Availability</Text>
-					<Progress rounded="0" value={20} mx='4' />
+					<Box>
+						<Text ml={5} >Temperature</Text>
+						<Progress value={65} width={'70%'} maxWidth={500} size={5}/>
+					</Box>
+					<Box>
+						<Text ml={5} >Humidity</Text>
+						<Progress value={50} width={'70%'} maxWidth={500} size={5}/>
+						</Box>
+					<Box>
+						<Text ml={5} >Water quality</Text>
+						<Progress value={80} width={'70%'} maxWidth={500} size={5}/>
+					</Box>
+					<Box>
+						<Text ml={5} >Water Availability</Text>
+						<Progress value={20} width={'70%'} maxWidth={500} size={5}/>
+					</Box>
+				</Stack>
+				<Box alignItems='center'>
+					<Button onPress={() => navigation.navigate('GrounwaterMap')} >Groundwater</Button>
 				</Box>
-			</Stack>
 		</SafeAreaView>
 	);
 }
--- src/GroundwaterMapView.tsx
+++ src/GroundwaterMapView.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, Center, Box } from 'native-base';
 
 export default function GroundwaterMapView() {
 	return (
-		<SafeAreaView>
-			
+		<SafeAreaView style={{ flex: 1 }}>
+			<Box flex={1} justifyContent="center" alignItems="center">
+				<Image
+					size={900}
+					resizeMode="contain"
+					source={require('../assets/gers-map-scale.png')}
+				/>
+			</Box>
 		</SafeAreaView>
 	);
 }
